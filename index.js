ce4531ef5f046aa13b18d835aaef6847f93e110c
CatsAreMLG <joshuadgon@gmail.com>
2019-03-16 11:19:43 -0500

    Add post and get endpoints

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express')
+const helmet = require('helmet')
+const knex = require('knex')
 
-const server = express();
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+}
+const db = knex(knexConfig)
 
-server.use(express.json());
-server.use(helmet());
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
 
 // endpoints here
 
-const port = 3300;
+//post
+server.post('/api/zoos', async (req, res) => {
+  if (req.body.name) {
+    try {
+      const [id] = await db('zoos').insert(req.body)
+      const zoo = await db('zoos')
+        .where({ id })
+        .first()
+      res.status(201).json(zoo)
+    } catch (e) {
+      const message = e[e.errno] || 'We ran into an error'
+      res.status(500).json({ message })
+    }
+  } else {
+    res.status(500).json({ message: 'Please provide a name' })
+  }
+})
+
+//get all
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos')
+    res.status(200).json(zoos)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+})
+
+const port = 3300
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`)
+})
